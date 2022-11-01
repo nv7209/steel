5eab1cfd8f8576ef8e833f8fe2944ea74d09e6be
Neeraj Verma <nv7209111@gmail.com>
2022-11-01 17:32:48 +0530

    final upload

---
+++ file-upload.php
@@ -0,0 +1,67 @@
+<?php 
+    // Database
+    include 'config/connection.php'; 
+
+
+
+
+
+    if(isset($_POST['submit'])){
+        
+        $uploadsDir = "uploads/";
+        $allowedFileType = array('jpg','png','jpeg','pdf','doc','docx','mp3');
+        
+        // Validate if files exist
+        if (!empty(array_filter($_FILES['fileUpload']['name']))) {
+            
+            // Loop through file items
+            foreach($_FILES['fileUpload']['name'] as $id=>$val){
+                // Get files upload path
+                $fileName        = $_FILES['fileUpload']['name'][$id];
+                $tempLocation    = $_FILES['fileUpload']['tmp_name'][$id];
+                $targetFilePath  = $uploadsDir . $fileName;
+                $fileType        = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
+                $uploadDate      = date('Y-m-d H:i:s');
+                $upload_upto     =$_POST["last-date"];
+                $uploadOk = 1;
+                if(in_array($fileType, $allowedFileType)){
+                        if(move_uploaded_file($tempLocation, $targetFilePath)){
+                            $sqlVal = "('".$fileName."', '".$uploadDate."','".$upload_upto."')";
+                        } else {
+                            $response = array(
+                                "status" => "alert-danger",
+                                "message" => "File coud not be uploaded."
+                            );
+                        }
+                    
+                } else {
+                    $response = array(
+                        "status" => "alert-danger",
+                        "message" => "Only .jpg, .jpeg, .pdf, .doc, .docx and .png file formats allowed."
+                    );
+                }
+                // Add into MySQL database
+                if(!empty($sqlVal)) {
+                    $insert = $con->query("INSERT INTO user (images, date_time,upload_upto) VALUES $sqlVal");
+                    if($insert) {
+                        $response = array(
+                            "status" => "alert-success",
+                            "message" => "Files successfully uploaded."
+                        );
+                    } else {
+                        $response = array(
+                            "status" => "alert-danger",
+                            "message" => "Files coudn't be uploaded due to database error."
+                        );
+                    }
+                }
+            }
+        } else {
+            // Error
+            $response = array(
+                "status" => "alert-danger",
+                "message" => "Please select a file to upload."
+            );
+        }
+    } 
+?>
